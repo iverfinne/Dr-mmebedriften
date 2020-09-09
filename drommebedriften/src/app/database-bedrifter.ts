e0a3a8a1f0b258892446ccc757ff37d4107b61d5
Iver.Finne <iverfinne@gmail.com>
2020-09-09 13:31:22 +0200

    Added text to companies

--- drommebedriften/src/app/database-bedrifter.ts
+++ drommebedriften/src/app/database-bedrifter.ts
@@ -23,11 +23,41 @@ function gfise(iframeEmbedLink?: string): string | null {
 // Alle Bedrifter - Lokal database
 export const Bedrifter: Bedrift[] = [
     {
+        namn: 'Tester',
+        ruterLink: 'tester',
+        logoLink: null,
+        framsidebileteLink: `${LocalPaths.path_bedriftFramsidebilete}evening-55067_1280.webp`,
+        beskriving1: 'tester1',
+        beskriving2: 'tester2',
+        linktekst1: 0,
+        link1: 'https://www.google.no/',
+        googleFormHTMLEmbedLink: gfise('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeKVBnAKvLDBBtZGVOJSq-v5WKxup3rWvkfM5ee2VmwADEa3w/viewform?embedded=true" width="640" height="378" frameborder="0" marginheight="0" marginwidth="0">Laster inn …</iframe>'),
+        tilleggskort: [
+            {
+                tittel: 'Test1',
+                beskriving1: 'Dette er ein test1.',
+                linktekst1: 'Custom link-tekst',
+                link1: 'https://google.no'
+            },
+            {
+                tittel: 'Test2',
+                beskriving1: 'Dette er ein test2.',
+                linktekst1: 0,
+                link1: 'https://google.no'
+            }
+        ]
+    },
+    {
         namn: 'DNV GL',
         ruterLink: 'dnv-gl',
         logoLink: `${LocalPaths.path_bedriftLogo}dnv_gl.png`,
         framsidebileteLink: `${LocalPaths.path_bedriftFramsidebilete}DNV-GL.jpg`,
-        beskriving1: ` sustainability in the customer's businesses.`,
+        beskriving1: `Siden 1864 har vårt formål vært å sikre liv, verdier og miljøet. Vi bistår våre kunder med å forbedre deres virksomhet på en sikker og bærekraftig måte. <br>
+        Vi leverer klassifisering, sertifisering, teknisk risiko- og pålitelighetsanalyse sammen med programvare, datahåndtering og uavhengig ekspertrådgivning til maritim sektor, til olje- og gass-sektoren, og til energibedrifter. Med 80,000 bedriftskunder på tvers av alle industrisektorer er vi også verdensledende innen sertifisering av ledelsessystemer. <br><br>
+        Ved å kombinere uavhengig teknisk, digital og operasjonell ekspertise med metoder for risikohåndtering, hjelper vi våre kunder med å ta bedre beslutninger og med å skape tillit mellom bedrifter og interessentene deres.
+<br><br>
+Vi utvikler nye industristandarder og bedre innsikt for våre kunder og samfunnet generelt ved å investere tungt i forskning og utvikling. Med høyt utdannede ansatte i 100 land, jobber vi sammen med våre kunder om å gjøre verden sikrere, smartere og grønnere.
+`,
         linktekst1: 0,
         link1: 'https://www.dnvgl.no/',
         googleFormHTMLEmbedLink: gfise('<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeKVBnAKvLDBBtZGVOJSq-v5WKxup3rWvkfM5ee2VmwADEa3w/viewform?embedded=true" width="640" height="378" frameborder="0" marginheight="0" marginwidth="0">Laster inn …</iframe>'),
@@ -97,7 +127,8 @@ export const Bedrifter: Bedrift[] = [
         ruterLink: 'tryg',
         logoLink: `${LocalPaths.path_bedriftLogo}Tryg.png`,
         framsidebileteLink: `${LocalPaths.path_bedriftFramsidebilete}Tryg.jpg`,
-        beskriving1: ``,
+        beskriving1: `Tryg er Nordens nest største skadeforsikringsselskap med aktiviteter i Danmark, Norge og Sverige. Vi har levert økonomisk og personlig sikkerhet for kundene våre i snart 300 år.
+        `,
         linktekst1: 0,
         link1: 'https://www.tryg.no/',
         googleFormHTMLEmbedLink: gfise(),
@@ -108,7 +139,8 @@ export const Bedrifter: Bedrift[] = [
         ruterLink: 'obos',
         logoLink: `${LocalPaths.path_bedriftLogo}OBOS.png`,
         framsidebileteLink: `${LocalPaths.path_bedriftFramsidebilete}Obos.jpg`,
-        beskriving1: ``,
+        beskriving1: `OBOS ble stiftet i 1929, og det første borettslaget sto ferdig på Etterstad to år etter. Helt siden den gang har vi skaffet våre medlemmer boliger og forvaltet disse på en trygg måte. Og det skal vi fortsette med – lenge!
+        `,
         linktekst1: 0,
         link1: 'https://www.obos.no/',
         googleFormHTMLEmbedLink: gfise(),
@@ -119,7 +151,8 @@ export const Bedrifter: Bedrift[] = [
         ruterLink: 'tekna',
         logoLink: `${LocalPaths.path_bedriftLogo}Tekna.png`,
         framsidebileteLink: `${LocalPaths.path_bedriftFramsidebilete}Tekna.png`,
-        beskriving1: ``,
+        beskriving1: `Har du, eller studerer du til, en master i naturvitenskap eller teknologi, kan du bli medlem i Tekna. Vi er Norges største fagforening for de med mastergrad, og tilbyr blant annet juridisk bistand, faglige tilbud og nettverk over hele landet, samt en av landets beste bank- og forsikringsavtaler.
+        `,
         linktekst1: 0,
         link1: 'https://www.tekna.no/',
         googleFormHTMLEmbedLink: gfise(),
@@ -130,7 +163,8 @@ export const Bedrifter: Bedrift[] = [
         ruterLink: 'accenture',
         logoLink: `${LocalPaths.path_bedriftLogo}Accenture.png`,
         framsidebileteLink: `${LocalPaths.path_bedriftFramsidebilete}Accenture.jpeg`,
-        beskriving1: ``,
+        beskriving1: `Vi kommer fra varierte bakgrunner, men jobber sammen. Med allsidigheten dette fører til, skal vi løse de største utfordringene selskaper, organisasjoner og etater står overfor. Dette gjør vi med entusiasme, kunnskap, innsikt og de aller siste og beste teknologiske løsningene. Vi har et bredt spekter av innovative verktøy, en læringskurve som aldri stanser og et globalt nettverk som gir oss en verden av erfaringer å høste av. Vi holder oss alltid i forkant; vi skaper endringer over alt.
+        `,
         linktekst1: 0,
         link1: 'https://www.accenture.no/',
         googleFormHTMLEmbedLink: gfise(),
@@ -141,7 +175,8 @@ export const Bedrifter: Bedrift[] = [
         ruterLink: 'forsvaret',
         logoLink: `${LocalPaths.path_bedriftLogo}Forsvaret.png`,
         framsidebileteLink: `${LocalPaths.path_bedriftFramsidebilete}Forsvaret.jpg`,
-        beskriving1: ``,
+        beskriving1: `Forsvaret har et bredt utdanningstilbud med mange muligheter. En utdanning i Forsvaret er ikke det samme som ved de fleste sivile høgskoler- og universiteter. Noen dager er det tradisjonell klasseromsundervisning, mens andre dager tilbringes ute på øvelse, på trening eller skytebanen. Forsvaret kombinerer teori og praksis på en måte som gir en spennende og variert skolehverdag.
+        `,
         linktekst1: 0,
         link1: 'https://www.forsvaret.no/',
         googleFormHTMLEmbedLink: gfise(),
@@ -152,7 +187,8 @@ export const Bedrifter: Bedrift[] = [
         ruterLink: 'knowit',
         logoLink: `${LocalPaths.path_bedriftLogo}Knowit.png`,
         framsidebileteLink: `${LocalPaths.path_bedriftFramsidebilete}Knowit.jpg`,
-        beskriving1: ``,
+        beskriving1: `Knowit AB (publ) er et konsulentselskap som, i den stadig raskere digitaliseringen, skaper unike kundeverdier gjennom å tilby grenseoverskridende leveranser fra de tre forretningsområdene Experience, Insight og Solutions. Det er evnen til å kombinere kompetanse innen design og kommunikasjon, management consulting og IT som skiller oss fra andre konsulentfirmaer. Vår kultur preges av åpenhet, forståelse for kundens forretninger, høy spesialistkompetanse og en vilje til å utvikles kontinuerlig. Knowit ble etablert i 1990 og har i dag ca. 2000 ansatte på 14 kontorer i Sverige, seks i Norge samt ett i Danmark, Finland og Tyskland. Knowit AB (publ) er notert på den Nordiska Börsen i Stockholm.
+        `,
         linktekst1: 0,
         link1: 'https://www.knowit.no/',
         googleFormHTMLEmbedLink: gfise(),
