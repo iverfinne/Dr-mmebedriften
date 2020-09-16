bdb71c60aa4d0e4ca0afef3fd6f49144e0d761ac
ravo10 <rassyrelling@gmail.com>
2020-09-16 23:30:57 +0200

    Tillaget basen for nedtelling

--- drommebedriften/src/app/nedteller-lansering/nedteller-lansering.component.ts
+++ drommebedriften/src/app/nedteller-lansering/nedteller-lansering.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nedteller-lansering.component.scss']
 })
 export class NedtellerLanseringComponent implements OnInit {
+  currNedtellingData: {
+    dager: number,
+    timer: number,
+    minutter: number,
+    sekunder: number
+  } = {
+      dager: 0,
+      timer: 0,
+      minutter: 0,
+      sekunder: 0
+    };
 
   constructor() { }
 
   ngOnInit(): void {
+    const targetDato = new Date('24. september 2020 00:00');
+
+    // Tell ned...
   }
 
 }
