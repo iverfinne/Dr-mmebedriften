d48875872d3f10b0647821b6c87b164cd906295a
ravo10 <rassyrelling@gmail.com>
2020-09-21 02:37:50 +0200

    Justerte funksjon for aa spore kor lenge ein brukar er  paa ein bedrift side

--- drommebedriften/src/app/ser/google-analytics.service.ts
+++ drommebedriften/src/app/ser/google-analytics.service.ts
@@ -42,63 +42,55 @@ export class GoogleAnalyticsService { /* https://firebase.google.com/docs/refere
    * Kor lenge ein brukar var på sida
   **/
   EinBedrift_korLengeEinBrukarVarPaSida(bedriftFintNamn: string, tidMS: number): void {
-    let cirkaTid = '<=0';
-    if (tidMS <= 1000) {
-      cirkaTid = '>0<=1000';
-    } else if (tidMS <= 3000) {
-      cirkaTid = '>1000<=3000';
-    } else if (tidMS <= 5000) {
-      cirkaTid = '>3000<=5000';
-    } else if (tidMS <= 7000) {
-      cirkaTid = '>5000<=7000';
-    } else if (tidMS <= 9000) {
-      cirkaTid = '>7000<=9000';
-    } else if (tidMS <= 11000) {
-      cirkaTid = '>9000<=11000';
-    } else if (tidMS <= 13000) {
-      cirkaTid = '>11000<=13000';
-    } else if (tidMS <= 15000) {
-      cirkaTid = '>13000<=15000';
-    } else if (tidMS <= 17000) {
-      cirkaTid = '>15000<=17000';
-    } else if (tidMS <= 19000) {
-      cirkaTid = '>17000<=19000';
-    } else if (tidMS <= 21000) {
-      cirkaTid = '>19000<=21000';
-    } else if (tidMS <= 29000) {
-      cirkaTid = '>21000<=29000';
-    } else if (tidMS <= 35000) {
-      cirkaTid = '>29000<=35000';
-    } else if (tidMS <= 40000) {
-      cirkaTid = '>35000<=40000';
-    } else if (tidMS <= 45000) {
-      cirkaTid = '>40000<=45000';
-    } else if (tidMS <= 50000) {
-      cirkaTid = '>45000<=50000';
-    } else if (tidMS <= 55000) {
-      cirkaTid = '>50000<=55000';
-    } else if (tidMS <= 65000) {
-      cirkaTid = '>55000<=65000';
-    } else if (tidMS <= 70000) {
-      cirkaTid = '>65000<=70000';
-    } else if (tidMS <= 75000) {
-      cirkaTid = '>70000<=75000';
-    } else if (tidMS <= 80000) {
-      cirkaTid = '>75000<=80000';
-    } else if (tidMS <= 90000) {
-      cirkaTid = '>80000<=90000';
-    } else if (tidMS <= 95000) {
-      cirkaTid = '>90000<=95000';
-    } else if (tidMS <= 100000) {
-      cirkaTid = '>95000<=100000';
-    } else {
-      cirkaTid = '>100000';
-    }
+    // Over 9000 !
+    if (tidMS > 9000) {
+      let cirkaTid = '>9000<=11000';
 
-    // *** Slått av... Bruker kanskje for mykje data...
-    /* this.googleAnalytics.logEvent('en_bedrift', {
-      hvor_lenge_paa_besok_msek: cirkaTid
-    }); */
+      if (tidMS > 100000) {
+        cirkaTid = '>100000';
+      } else if (tidMS > 95000) {
+        cirkaTid = '>95000<=100000';
+      } else if (tidMS > 90000) {
+        cirkaTid = '>90000<=95000';
+      } else if (tidMS > 80000) {
+        cirkaTid = '>80000<=90000';
+      } else if (tidMS > 75000) {
+        cirkaTid = '>75000<=80000';
+      } else if (tidMS > 70000) {
+        cirkaTid = '>70000<=75000';
+      } else if (tidMS > 65000) {
+        cirkaTid = '>65000<=70000';
+      } else if (tidMS > 55000) {
+        cirkaTid = '>55000<=65000';
+      } else if (tidMS > 50000) {
+        cirkaTid = '>50000<=55000';
+      } else if (tidMS > 45000) {
+        cirkaTid = '>45000<=50000';
+      } else if (tidMS > 40000) {
+        cirkaTid = '>40000<=45000';
+      } else if (tidMS > 35000) {
+        cirkaTid = '>35000<=40000';
+      } else if (tidMS > 29000) {
+        cirkaTid = '>29000<=35000';
+      } else if (tidMS > 21000) {
+        cirkaTid = '>21000<=29000';
+      } else if (tidMS > 19000) {
+        cirkaTid = '>19000<=21000';
+      } else if (tidMS > 17000) {
+        cirkaTid = '>17000<=19000';
+      } else if (tidMS > 15000) {
+        cirkaTid = '>15000<=17000';
+      } else if (tidMS > 13000) {
+        cirkaTid = '>13000<=15000';
+      } else if (tidMS > 11000) {
+        cirkaTid = '>11000<=13000';
+      }
+
+      // *** Slått av... Bruker kanskje for mykje data...
+      this.googleAnalytics.logEvent('en_bedrift', {
+        hvor_lenge_paa_besok_msek: cirkaTid
+      });
+    }
   }
 
   // *****************
